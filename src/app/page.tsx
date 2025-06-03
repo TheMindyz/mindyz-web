55dcd7ee083e0b3ab5aba75caaea2b46f4aff83d
TheMindyz <equipemindyz@gmail.com>
2025-06-03 13:39:17 -0300

    atualização

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1146,7 +1146,7 @@ onClick={() => alert(`Check-in registrado: ${label}`)}
     <div className="particles"></div>
 
     {/* 🔥 Conteúdo Premium */}
-    <section className="relative z-10 w-[90%] max-w-4xl min-h-[600px] bg-zinc-900/90 border-4 border-green-500 rounded-3xl shadow-2xl backdrop-blur-md flex flex-col items-center justify-center p-10 space-y-6 text-center">
+     <section className="relative z-10 w-[90%] max-w-4xl min-h-[600px] bg-zinc-900/90 border-[5px] border-green-400 rounded-3xl shadow-[0_0_40px_#22c55eaa] backdrop-blur-md flex flex-col items-center justify-center p-10 space-y-6 text-center">
       <BotaoVoltar voltarPara="home" />
 
       <h2 className="text-5xl font-extrabold text-green-400 animate-pulse">
