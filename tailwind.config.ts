a7eb57532feca4047671f7f5df3a2262f41174be
TheMindyz <equipemindyz@gmail.com>
2025-06-11 12:18:01 -0300

    atualização

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-const config = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx}',
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-export default config
+};
