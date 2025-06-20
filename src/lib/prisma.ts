bee761cf2a360745791f9cb593799b088767259d
TheMindyz <equipemindyz@gmail.com>
2025-06-20 19:03:13 -0300

    atualização

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,14 @@
+// src/lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ["query"], // opcional, útil para debug
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
