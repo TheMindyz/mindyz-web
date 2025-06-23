1a9f0a55066e8b8ed9ee83715854e6a75bcc3f6e
TheMindyz <equipemindyz@gmail.com>
2025-06-23 19:59:33 -0300

    Corrige API de premium-status para aceitar GET

--- src/app/api/premium-status/premium-status.ts
+++ src/app/api/premium-status/premium-status.ts
@@ -1,3 +1,5 @@
+// src/app/api/premium-status/route.ts
+
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "@/lib/prisma";
 
@@ -5,21 +7,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== "POST") {
+  // Troca de POST para GET
+  if (req.method !== "GET") {
     return res.status(405).json({ error: "Método não permitido" });
   }
 
   try {
-    const { email } = req.body;
+    const email = req.query.email as string;
 
     if (!email) {
       return res
         .status(400)
-        .json({ error: "E-mail ausente no corpo da requisição" });
+        .json({ error: "E-mail ausente na query da requisição" });
     }
 
     const pagamento = await prisma.pagamento.findUnique({ where: { email } });
-
     const isPremium = pagamento?.status === "ativo";
 
     return res.status(200).json({ isPremium });
